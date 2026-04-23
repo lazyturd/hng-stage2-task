bf556fb969eaba8f1e9ca0d4e7d4429a7c80d1da
Ayomide <ayorexyshittu@gmail.com>
2026-04-24 00:37:52 +0100

    ok

--- worker/worker.py
+++ worker/worker.py
@@ -2,13 +2,11 @@ import redis
 import time
 import os
 
-
 r = redis.Redis(
     host=os.getenv("REDIS_HOST", "redis"),
     port=int(os.getenv("REDIS_PORT", 6379)),
     password=os.getenv("REDIS_PASSWORD", None)
 )
-
 
 
 def process_job(job_id):
